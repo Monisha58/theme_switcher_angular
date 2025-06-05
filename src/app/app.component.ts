9f821c5d82873f652ee8bdc759c2a5823749c227
Monisha Sivanthilingam <monisha.s@kaspontech.com>
2025-06-05 20:02:36 +0530

    theme change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, ViewEncapsulation } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'theme_switcher';
+  theme: string = 'light';
+  themes: any = [
+    { color: 'light', text: 'Light' },
+    { color: 'light-contrast', text: 'Light Contrast' },
+    { color: 'dark', text: 'Dark' },
+    { color: 'dark-contrast', text: 'Dark Contrast' },
+  ]
+
+  constructor(
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  switch() {
+    const body = document.body;
+    body.className = this.theme;
+    // if(this.theme == 'light') {
+    //   body.classList.remove('dark');
+    //   body.classList.add('light');
+    // } else if(this.theme == 'dark') {
+    //   body.classList.remove('light');
+    //   body.classList.add('dark');
+    // }
+    this.cdr.detectChanges();
+  }
 }
